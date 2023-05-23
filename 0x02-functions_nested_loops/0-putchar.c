9150dfbebb206811e448f2324ae9a865327efe9c
luckysitara <udoessienprecious@gmail.com>
2023-05-23 05:24:26 -0700

    writting code for 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main_h"
+int main(void)
+{
+/* printing "_putchar" using _putchar function */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+/* return on success 0 */
+	return (0);
+}
