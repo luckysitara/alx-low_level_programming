7916e7cd8eb8cf7aa0f8d3d03813abaae519fc55
luckysitara <udoessienprecious@gmail.com>
2023-05-24 14:14:18 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,20 +4,19 @@
  *@n: the sign to print
  * Return: 1 and print + if n is  > zer oAlways 0.
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
