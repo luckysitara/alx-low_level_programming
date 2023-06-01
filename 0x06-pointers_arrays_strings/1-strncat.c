ab8fe4c87a28ef0c4ea48478c745706805c0a493
luckysitara <udoessienprecious@gmail.com>
2023-06-01 01:34:37 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,3 +15,17 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
 
+	a = b = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
