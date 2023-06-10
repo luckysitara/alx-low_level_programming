84970b983c9a31ad2052417be43ef670642bfd2e
luckysitara <udoessienprecious@gmail.com>
2023-06-09 20:56:56 -0700

    3-mul.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,11 @@
 #ifndef main_h
 #define main_h
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char **argv __attribute__((unused)));
+int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 
 #endif
