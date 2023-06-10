b8b474f817dd8558d45f78fa509dcf830b67d2e6
luckysitara <udoessienprecious@gmail.com>
2023-06-09 21:16:09 -0700

    100-change.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -9,5 +9,6 @@ int main(int argc, char **argv __attribute__((unused)));
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
