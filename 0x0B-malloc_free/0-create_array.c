a6af6431510ea5cf760b3630f8259bf3191a5029
Bughacker Calculus Janaan <bughacker@msf-venom.evileye>
2023-06-13 19:24:41 -0400

    0-create_array.c correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * create_array - creates an array of chars and initializes it with a specific char
- * @size: size of the array
- * @c: char to initialize the array with
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  *
- * Return: pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
-	unsigned int a;
+	char *str;
+	unsigned int i;
 
-	arr = malloc(sizeof(char) * size);
-	if(size == 0 || arr == NULL)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	for (a = 0; a < size; a++)
-		arr[a] = c;
-	return (arr);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
