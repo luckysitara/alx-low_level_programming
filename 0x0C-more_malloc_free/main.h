44b2a961b29ecffd6444e1f5a88472a34f315cbd
luckysitara <udoessienprecious@gmail.com>
2023-06-15 14:35:52 -0700

    fixing _realloc prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <limits.h>
 
 void *malloc_checked(unsigned int b);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
