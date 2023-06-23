e8636885e2335702de792dbfceb62c5817f02076
luckysitara <udoessienprecious@gmail.com>
2023-06-23 03:49:14 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-
-/*	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}*/
 	while (i < size)
 	{
 		action(array[i]);
