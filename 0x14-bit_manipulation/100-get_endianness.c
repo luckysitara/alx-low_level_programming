f4bc7af0219bfa91789b3c12af5c9a4433ff2f60
luckysitara <udoessienprecious@gmail.com>
2023-07-24 05:11:44 -0700

    6-endianess_bit.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,20 @@
 #include "main.h"
+/**
+ * get_endianness - Check the endianess
+ *
+ * Return: void
+ */
+
+int get_endianness(void)
+{
+	int iindex;
+
+	iindex = 2;
+
+	if (iindex & 1)
+	{
+		return (0);
+	}
+
+	return (1);
+}
