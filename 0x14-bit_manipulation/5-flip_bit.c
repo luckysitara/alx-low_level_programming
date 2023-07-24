d8e73ace07e3dc94c13a8cf3ffbfc90986fd4a0a
luckysitara <udoessienprecious@gmail.com>
2023-07-24 05:05:40 -0700

    5-flip_bit.c

--- 0x14-bit_manipulation/5-flip_bit.c
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -1,2 +1,27 @@
 #include "main.h"
+/**
+ * flip_bits - Number of bits needed to flip to get from one number to another.
+ *
+ * @n: long int
+ *
+ * @m: unsigned int
+ *
+ * Return: 1 if works -1 on failure
+ */
 
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int ms = 0, index;
+
+	index = n ^ m;
+
+	while (index > 0)
+	{
+		if (index & 1)
+		{
+			ms++;
+		}
+		index >>= 1;
+	}
+	return (ms);
+}
